5db5683f0261895727f4e2ab3a07542953519183
Shogo Sawai <shogo.sawai@gmail.com>
2023-05-10 11:34:24 +0900

    support conf file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ struct Args {
     #[arg(short, long)]
     conf_str: Option<String>,
 
+    #[arg(long)]
+    conf_file: Option<String>,
+
     #[arg(short, long, default_value = "NGINX_CONF")]
     env_var: String,
 
@@ -19,6 +22,8 @@ impl Args {
     fn get_conf(&self) -> String {
         if let Some(conf_str) = &self.conf_str {
             conf_str.clone()
+        } else if let Some(conf_file) = &self.conf_file {
+            std::fs::read_to_string(conf_file).expect("failed to read file")
         } else {
             std::env::var(&self.env_var)
                 .ok()
