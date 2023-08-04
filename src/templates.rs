c4fe571a231ef0540b6e4fd4320130fa8174dbd0
Shogo Sawai <shogo.sawai@gmail.com>
2023-08-04 16:16:26 +0900

    bug fix

--- src/templates.rs
+++ src/templates.rs
@@ -101,6 +101,10 @@ mod tests {
     proxy_pass http://app:8000/;
     proxy_set_header Host $host;
     proxy_set_header X-Real-IP $remote_addr;
+    proxy_redirect off;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection "upgrade";
     try_files $uri $uri/ / =404;
   }"#
         );
