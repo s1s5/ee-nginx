8a1ed9e34271e6facc7070f5340112d36221c02e
Shogo Sawai <shogo.sawai@gmail.com>
2026-05-28 09:44:26 +0900

    :bug: fix watch target: config file instead of its directory (RecursiveMode caused PermissionDenied on /proc)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [ -n "${NGINX_CONF_FILE:-}" ]; then
     if [ "${NGINX_AUTO_RELOAD:-false}" = "true" ]; then
         # Watch the directory containing the config file for changes
         CONF_DIR="$(dirname "$NGINX_CONF_FILE")"
-        /app/generator --conf-file "$NGINX_CONF_FILE" --watch "$CONF_DIR" --reload-nginx &
+        /app/generator --conf-file "$NGINX_CONF_FILE" --watch "$NGINX_CONF_FILE" --reload-nginx &
     else
         /app/generator --conf-file "$NGINX_CONF_FILE"
     fi
